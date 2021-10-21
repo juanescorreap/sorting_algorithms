8bb9010300a1965af5a6a8331db6a9061fd7782a
John Doe <juanestebancope@outlook.com>
2021-10-21 11:03:10 -0500

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm
+ *
+ * @list: Doubly linked list
+ * Return: Void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i = *list;
+	listint_t *j = *list;
+	const int key;
+
+	if (!h)
+	{
+		return;
+	}
+	while (i)
+	{
+		key = i->n;
+
+	}
