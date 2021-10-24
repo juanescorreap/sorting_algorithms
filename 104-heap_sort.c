66cc681d8582c4a0a98daabc2273370754edeb08
JuanDavidPena <j_david0915@hotmail.com>
2021-10-24 04:46:19 +0000

    [fixed]-sift_down

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,14 +1,14 @@
 #include "sort.h"
 
 /**
- * heapify - Sorts an array on a max heap recursively.
+ * sift_down - Sorts an array on a max heap recursively.
  * @array: Array to be sorted.
  * @n: size of the array to heap.
  * @i: index to star the heap from.
  * @big_size: size of the complete array to print.
  * Return: void.
  */
-void heapify(int array[], int n, int i, int big_size)
+void sift_down(int array[], int n, int i, int big_size)
 {
 	int largest = i;
 	int l = 2 * i + 1;
@@ -27,7 +27,7 @@ void heapify(int array[], int n, int i, int big_size)
 		array[i] = array[largest];
 		array[largest] = tmp;
 		print_array(array, big_size);
-		heapify(array, n, largest, big_size);
+		sift_down(array, n, largest, big_size);
 	}
 }
 
@@ -42,7 +42,7 @@ void heap_sort(int *array, size_t size)
 	int tmp = 0;
 
 	for (int i = size / 2 - 1; i >= 0; i--)
-		heapify(array, size, i, size);
+		sift_down(array, size, i, size);
 
 	for (int i = size - 1; i > 0; i--)
 	{
@@ -50,6 +50,6 @@ void heap_sort(int *array, size_t size)
 		array[0] = array[i];
 		array[i] = tmp;
 		print_array(array, size);
-		heapify(array, i, 0, size);
+		sift_down(array, i, 0, size);
 	}
 }
