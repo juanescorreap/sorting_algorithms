447cb23365c779e2eea70b6f59bd25ec60555074
JuanDavidPena <j_david0915@hotmail.com>
2021-10-25 19:04:20 +0000

    [modify]- used malloc

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 
-void countSort(int *array, int size, int exp)
+void countSort(int *array, int size, int exp, int *output)
 {
-	int output[size];
-	int i, count[10] = { 0 };
+	int i;
+	int count[10] = {0,0,0,0,0,0,0,0,0,0};
 
 	for (i = 0; i < (int)size; i++)
 		count[(array[i] / exp) % 10]++;
@@ -27,14 +27,19 @@ void radix_sort(int *array, size_t size)
 {
 	int max = array[0];
 	int i, exp;
+	int *output = NULL;
 
 	for (i = 1; i < (int)size; i++)
 		if (array[i] > max)
 			max = array[i];
 
+	output = malloc(sizeof(int) * max);
+	if (!output)
+		return;
 	for (exp = 1; max / exp > 0; exp *= 10)
 	{
-		countSort(array, (int)size, exp);
+		countSort(array, (int)size, exp, output);
 		print_array(array, size);
 	}
+	free(output);
 }
